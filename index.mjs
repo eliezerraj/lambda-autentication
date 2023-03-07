e1c4512cbe2049af51141b3db0f2811bbfb7a3d9
eliezerraj <eliezerraj@gmail.com>
2023-03-07 16:17:50 -0300

    fix bugs

--- index.mjs
+++ index.mjs
@@ -18,8 +18,8 @@ var statusCode = 200;
 var msg = {"message": "Succesfull"};
 
 const headers = { 
-"Content-Type": "application/json", 
-"Access-Control-Allow-Origin" : "*" 
+  "Content-Type": "application/json", 
+  "Access-Control-Allow-Origin" : "*" 
 };
 
 var encrypt_token;
@@ -37,55 +37,60 @@ const context = {
     origin: 'us-east-2',
 };
 
+var scope;
+var token;
+
 // -------------------------------    
 const db_users = {
-user1: "password1",
-user2: "password2",
+  user1: "password1",
+  user2: "password2",
 };
 // -------------------------------    
 
 export const handler = async(event) => {
   console.log('*** Loading lambda autentication Version 1');
-console.log("***********************");
-console.log(event);
-console.log("***********************");
+  console.log("***********************");
+  console.log(event);
+  console.log("***********************");
 
-const { user, password } = event.body;
+  const { user, password } = event.body;
 
-console.log("db_users[user]  : ", db_users[user] );
+  console.log("user  : ", user );
+  console.log("password  : ", password );
+  console.log("db_users[user]  : ", db_users[user] );
 
-if (!user || !password || db_users[user] !== password) {
-  statusCode = 401;
-  msg.message = "User or Password Inválid";
-}
+  if (!user || !password || db_users[user] !== password) {
+    statusCode = 401;
+    msg.message = "User or Password Inválido";
+  } else {
+    scope = {
+      scope: [ "openid", "profile", "email", "offline_access" ],
+    };
+  
+    token =  jwt.sign({ user , scope},
+                              jwtKey, {
+                                  algorithm: "HS256",
+                                  expiresIn: jwtExpirySeconds,
+                              });
+          
+    console.log("-----------------------------------------");
+    console.log("token:", token);
+  
+    encrypt_token = await encrypt(keyring, token, { encryptionContext: context });
+    //console.log("-----------------------------------------");
+    //console.log("encrypt_token:", encrypt_token);
+    //console.log("-----------------------------------------");
+    //console.log("encrypt_token.result:", encrypt_token.result.toString('base64'));
+    //console.log("-----------------------------------------");
+  }
 
-const scope = {
-  scope: [ "openid", "profile", "email", "offline_access" ],
-};
+  const response = {
+    statusCode: statusCode,
+    headers: headers,
+    token: token,
+    encrypt_token: encrypt_token.result.toString('base64'),
+    body: msg,
+  };
 
-const token =   jwt.sign({ user , scope},
-                          jwtKey, {
-                              algorithm: "HS256",
-                              expiresIn: jwtExpirySeconds,
-                          });
-      
-console.log("-----------------------------------------");
-console.log("token:", token);
-
-encrypt_token = await encrypt(keyring, token, { encryptionContext: context });
-//console.log("-----------------------------------------");
-//console.log("encrypt_token:", encrypt_token);
-//console.log("-----------------------------------------");
-//console.log("encrypt_token.result:", encrypt_token.result.toString('base64'));
-//console.log("-----------------------------------------");
-
-const response = {
-  statusCode: statusCode,
-  headers: headers,
-  token: token,
-  encrypt_token: encrypt_token.result.toString('base64'),
-  body: msg,
-};
-
-return response;
+  return response;
 };
